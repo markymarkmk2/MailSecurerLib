4ba7bd90ff7f2152f5a2762fcdd3cce3589d62ac
Mark Williams <mark@dimm.de>
2010-03-24 13:21:43 +0000

    No Output

--- src/home/shared/Utilities/DefaultTrustManager.java
+++ src/home/shared/Utilities/DefaultTrustManager.java
@@ -9,13 +9,13 @@ public class DefaultTrustManager implements X509TrustManager {
 
     public void checkClientTrusted(X509Certificate[] cert, String authType) 
     {
-        System.out.println("C: trusting " + authType);
+        //System.out.println("C: trusting " + authType);
 	// everything is trusted
     }
 
     public void checkServerTrusted(X509Certificate[] cert, String authType)
     {
-        System.out.println("S: trusting " + authType);
+        //System.out.println("S: trusting " + authType);
 	// everything is trusted
     }
 
