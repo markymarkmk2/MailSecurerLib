2ef502e73c768ddf5e7842c704a86cc13c2eabe0
Mark Williams <mark@dimm.de>
2010-08-25 08:55:52 +0000

    New

---
+++ src_124/home/shared/Utilities/LogConfigEntry.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package home.shared.Utilities;
+
+/**
+ *
+ * @author mw
+ */
+public class LogConfigEntry
+{
+    public String typ;
+    public int level;
+
+    public LogConfigEntry( String typ, int level )
+    {
+        this.typ = typ;
+        this.level = level;
+    }
+
+
+
+}
